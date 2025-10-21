e6c66f4b4375a0cff237ffa784b6d7d9116537db
HarshitaSalvi <harshitasalvi220480@acropolis.in>
2025-10-22 01:49:52 +0530

    Added server side rendering

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require('path');
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRoute")
 const URL = require("./models/url");
 const { connectMongoDB } = require("./connection");
 
@@ -7,12 +9,23 @@ const app = express();
 const PORT = 8001;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false})); //to parse form data
+
+app.set('view engine', "ejs");
+app.set('views', path.resolve("./views"));
 
 connectMongoDB("mongodb://localhost/short-url").then(() => {
   console.log("MongoDB connected...");
 });
 
+app.get("/test", async (req, res)=>{
+    // res.end(`<h1> Hello from Harshu </h1>`)
+    const allUrls = await URL.find({});
+    return res.render("home", {urls : allUrls});
+})
+
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortURL = req.params.shortId;
